0254d0407d4486c117dd34a84ad45ee56d1083a2
Artem Sivolodskii <neo_mat@inbox.ru>
2019-04-19 23:25:07 +1000

    Task ready

--- public_html/classes/components/custom.php
+++ public_html/classes/components/custom.php
@@ -9,4 +9,68 @@
 		public function __call($method, $args) {
 			throw new publicException('Method ' . get_class($this) . '::' . $method . " doesn't exist");
 		}
+
+		public function sendback(){
+
+			$result ='';
+
+			$Name = htmlspecialchars(getRequest('Name'));
+
+			$Email = getRequest('Email');
+
+			$Comment = htmlspecialchars(getRequest('Comment'));
+
+			if (!filter_var($Email, FILTER_VALIDATE_EMAIL)) {
+
+    				$result .= "<- Incorrect email value ->";
+
+				}
+
+
+			if (empty($Name)){
+
+    				$result .= "<- Empty name value ->";
+
+				}
+
+			if (empty($Comment)){
+
+    				$result .= "<- Empty comment value ->";
+    				
+				}	
+
+
+
+			if($result==''){
+
+			$mail = new umiMail;
+ 
+ 			$mail->addRecipient("neo_mat@inbox.ru", "Support");
+ 
+ 			$mail->setFrom($Email, $Name);
+ 
+ 			$mail->setSubject("FeedBack");
+ 
+ 			$mail->setContent($Comment);
+
+ 			$mail->commit();
+ 
+ 			$mail->send();
+
+			return "ok";
+
+
+			}else{
+
+			return $result;
+
+			}
+
+
+
+			
+		}
+
+
+
 	}
